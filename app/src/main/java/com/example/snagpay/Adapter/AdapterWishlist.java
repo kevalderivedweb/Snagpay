51d2b6d217e4b2fefabab33fcb4100baa58c8fc2
Nimish Mer <mer.nimish@gmail.com>
2021-01-28 16:28:54 +0530

    ok

--- app/src/main/java/com/example/snagpay/Adapter/AdapterWishlist.java
+++ app/src/main/java/com/example/snagpay/Adapter/AdapterWishlist.java
@@ -1,21 +1,30 @@
 package com.example.snagpay.Adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.snagpay.Model.CategoryDetailsModel;
 import com.example.snagpay.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 public class AdapterWishlist extends RecyclerView.Adapter<AdapterWishlist.Viewholder> {
 
     private Context mContext;
+    private ArrayList<CategoryDetailsModel> categoryDetailsModelArrayList;
 
-    public AdapterWishlist(Context mContext) {
+    public AdapterWishlist(Context mContext, ArrayList<CategoryDetailsModel> categoryDetailsModelArrayList) {
         this.mContext = mContext;
+        this.categoryDetailsModelArrayList = categoryDetailsModelArrayList;
     }
 
     @NonNull
@@ -29,16 +38,34 @@ public class AdapterWishlist extends RecyclerView.Adapter<AdapterWishlist.Viewho
     @Override
     public void onBindViewHolder(@NonNull AdapterWishlist.Viewholder holder, int position) {
 
+        Picasso.get().load(categoryDetailsModelArrayList.get(position).getDeal_image()).into(holder.imgWishlist);
+
+        holder.titleWishlist.setText(categoryDetailsModelArrayList.get(position).getTitle());
+        holder.boughtWishlist.setText(categoryDetailsModelArrayList.get(position).getBought());
+        holder.priceWishlist.setText(categoryDetailsModelArrayList.get(position).getSell_price());
+
+        Log.e("logoo", categoryDetailsModelArrayList.get(position).getTitle() + "--" + categoryDetailsModelArrayList.get(position).getBought() +
+                "--" + categoryDetailsModelArrayList.get(position).getSell_price());
     }
 
     @Override
     public int getItemCount() {
-        return 2;
+        return categoryDetailsModelArrayList.size();
     }
 
     public class Viewholder extends RecyclerView.ViewHolder {
+
+        ImageView imgWishlist;
+        TextView titleWishlist, boughtWishlist, priceWishlist;
+
         public Viewholder(@NonNull View itemView) {
             super(itemView);
+
+            imgWishlist = itemView.findViewById(R.id.imgWishlist);
+            titleWishlist = itemView.findViewById(R.id.titleWishlist);
+            boughtWishlist = itemView.findViewById(R.id.boughtWishlist);
+            priceWishlist = itemView.findViewById(R.id.priceWishlist);
+
         }
     }
 }
--- app/src/main/java/com/example/snagpay/Fragments/Fragment_WishListAcivity.java
+++ app/src/main/java/com/example/snagpay/Fragments/Fragment_WishListAcivity.java
@@ -5,17 +5,36 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+import com.example.snagpay.API.VolleyMultipartRequest;
 import com.example.snagpay.Adapter.AdapterWishlist;
 import com.example.snagpay.Adapter.AdapterWishlistRecentViewed;
+import com.example.snagpay.Model.CategoryDetailsModel;
 import com.example.snagpay.R;
 import com.example.snagpay.Utils.UserSession;
+import com.kaopiz.kprogresshud.KProgressHUD;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Fragment_WishListAcivity extends Fragment {
 
@@ -26,6 +45,8 @@ public class Fragment_WishListAcivity extends Fragment {
     private RelativeLayout rltvWishListTopBar, rltvWishListTopBarDeleteCancel;
     private TextView txtEditWishlistTopBar;
     private ImageView imgCancelWishlishTopBar;
+
+    private ArrayList<CategoryDetailsModel> categoryDetailsModelArrayList = new ArrayList<>();
 
     private UserSession session;
 
@@ -49,13 +70,16 @@ public class Fragment_WishListAcivity extends Fragment {
         resFragWishList = view.findViewById(R.id.resFragWishList);
         resFragRecentlyViewed = view.findViewById(R.id.resFragRecentlyViewed);
 
+        getWishlist();
+
         resFragWishList.setLayoutManager(new LinearLayoutManager(getContext()));
-        adapterFragWishlist = new AdapterWishlist(getActivity());
+        adapterFragWishlist = new AdapterWishlist(getActivity(), categoryDetailsModelArrayList);
         resFragWishList.setAdapter(adapterFragWishlist);
 
         resFragRecentlyViewed.setLayoutManager(new LinearLayoutManager(getContext()));
         adapterFragWishlistRecentViewed = new AdapterWishlistRecentViewed(getActivity());
         resFragRecentlyViewed.setAdapter(adapterFragWishlistRecentViewed);
+
 
         // for top bar hidden
         txtEditWishlistTopBar.setOnClickListener(new View.OnClickListener() {
@@ -75,6 +99,119 @@ public class Fragment_WishListAcivity extends Fragment {
         });
 
         return view;
+    }
+
+    private void getWishlist(){
+        final KProgressHUD progressDialog = KProgressHUD.create(getContext())
+                .setStyle(KProgressHUD.Style.SPIN_INDETERMINATE)
+                .setLabel("Please wait")
+                .setCancellable(false)
+                .setAnimationSpeed(2)
+                .setDimAmount(0.5f);
+        //.show();
+        //getting the tag from the edittext
+
+        //our custom volley request
+        VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.GET, session.BASEURL + "get-wishlist",
+                new Response.Listener<NetworkResponse>() {
+            @Override
+            public void onResponse(NetworkResponse response) {
+
+                progressDialog.dismiss();
+
+                try {
+
+                    JSONObject jsonObject = new JSONObject(new String(response.data));
+                    Log.e("Response",jsonObject.toString());
+                    if (jsonObject.getString("ResponseCode").equals("200")){
+
+                        try {
+
+                            JSONObject data = jsonObject.getJSONObject("data");
+                            JSONObject jsonObject1 = data.getJSONObject("wishlist");
+                            JSONArray sub_categories = data.getJSONArray("recently_viewed");
+
+                            JSONArray jsonArray = jsonObject1.getJSONArray("data");
+
+                            for (int i = 0 ; i<jsonArray.length() ; i++){
+                                JSONObject object = jsonArray.getJSONObject(i);
+
+                                CategoryDetailsModel categoryDetailsModel = new CategoryDetailsModel();
+                                categoryDetailsModel.setDeal_image(object.getString("deal_image"));
+                                categoryDetailsModel.setTitle(object.getString("title"));
+                                categoryDetailsModel.setSell_price(object.getString("sell_price"));
+                                categoryDetailsModel.setBought(object.getString("bought"));
+                                categoryDetailsModel.setMain_category_id(object.getString("main_category_id"));
+                                categoryDetailsModel.setCategory_id(object.getString("category_id"));
+
+                                categoryDetailsModelArrayList.add(categoryDetailsModel);
+
+                            }
+
+                            Log.e("ree", categoryDetailsModelArrayList.get(0).getTitle() + "--" + categoryDetailsModelArrayList.get(0).getSell_price()
+                                    + categoryDetailsModelArrayList.get(0).getBought());
+
+                            Log.e("daaaaata", jsonObject1.toString() + "  --");
+
+                            adapterFragWishlist.notifyDataSetChanged();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Log.e("rsp", e.getMessage() + "--");
+                        }
+                    }
+
+                } catch (Exception e) {
+                    //  Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getContext(), "No Data", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.dismiss();
+
+                        Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+
+            /*
+             * If you want to add more parameters with the image
+             * you can do it here
+             * here we have only one parameter with the image
+             * which is tags
+             * */
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+
+                return params;
+            }
+
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Accept", "application/json");
+                params.put("Authorization", "Bearer " + session.getAPITOKEN());
+                return params;
+            }
+
+            /*
+             * Here we are passing image by renaming it with a unique name
+             * */
+            @Override
+            protected Map<String, DataPart> getByteData() {
+                Map<String, DataPart> params = new HashMap<>();
+
+                return params;
+            }
+        };
+        //adding the request to volley
+        Volley.newRequestQueue(getContext()).add(volleyMultipartRequest);
     }
 
     @Override
